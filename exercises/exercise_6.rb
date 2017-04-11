caec1acb3273d792d1ba3ef501bd2840290f1220
Noel Anstey <anstey.ng@gmail.com>
2017-04-11 15:25:39 -0700

    exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,9 +10,9 @@ puts "----------"
 
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Noel", last_name: "Anstey", hourly_rate: 25)
-@store1.employees.create(first_name: "Paige", last_name: "Kato", hourly_rate: 30)
+@store1.employees.create(first_name: "Noel", last_name: "Anstey", hourly_rate: 45)
+@store1.employees.create(first_name: "Paige", last_name: "Kato", hourly_rate: 50)
 
-@store2.employees.create(first_name: "Chester", last_name: "M'Lester", hourly_rate: -50)
+@store2.employees.create(first_name: "Chester", last_name: "M'Lester", hourly_rate: 50)
 @store2.employees.create(first_name: "Dickens", last_name: "McSchlong", hourly_rate: 40)
-@store2.employees.create(first_name: "Hugh", last_name: "Bixley", hourly_rate: 50)
+@store2.employees.create(first_name: "Hugh", last_name: "Bixley", hourly_rate: 60)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :store_must_have_apparel
+
+  def store_must_have_apparel
+    unless ( mens_apparel == true || womens_apparel == true )
+      errors.add(:mens_apparel, "Store must sell either mens_apparel or womens_apparel or both")
+    end
+  end
 end
