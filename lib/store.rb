4c429c51fcd598a740098bb90244e43ffdfe70e8
Noel Anstey <anstey.ng@gmail.com>
2017-04-11 17:31:04 -0700

    exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,5 @@
 class Store < ActiveRecord::Base
-  has_many :employees
+  has_many :employees, :dependent => :restrict_with_error
   validates :name, length: {minimum: 3}
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
   validate :store_must_have_apparel
